a8bba24f485e4f8e3ee74567296e77f2753ad4af
makotovh <makoto@makoto.blog.br>
2018-08-19 23:52:22 -0300

    Implement interfaces for services. closes #5

---
+++ petclinic-data/src/main/java/in/makot/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package in.makot.springpetclinic.services;
+
+import in.makot.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ petclinic-data/src/main/java/in/makot/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package in.makot.springpetclinic.services;
+
+import in.makot.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
